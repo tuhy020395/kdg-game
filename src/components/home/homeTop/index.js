0024c4056a48774a0a4aba8812019ff9562864b7
cuongson <Administrator@DESKTOP-2021QTC>
2021-10-07 01:57:13 +0700

    update home page

---
+++ src/components/home/homeTop/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import 'antd/dist/antd.css';
+import './homeTop.css'
+
+function HomeTop() {
+    return (
+
+        <div className='banner'>
+            <div className='imgBanner' >
+                <img src='../images/textBanner.png' alt='' />
+                
+
+            </div>
+            <div className='textBanner'>
+                    <p className='text'>A cutting-edge gamefi laundpad and huge game market for gamers</p>
+                </div>
+
+            <div className='btnBanner'>
+                <button className='btn' style={{border:'none', background:'none'}} >
+                    <img src='../images/icon.png' alt='' />
+                </button>
+            </div>
+
+
+        </div>
+
+    );
+}
+
+export default HomeTop;
